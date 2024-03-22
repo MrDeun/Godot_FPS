25673f3f19736a8aee8e6413dd02f8a3e5a1fedf
Patryk Michalak <pmichalak16@gmail.com>
2024-03-22 11:18:56 +0100

    Added alt texture for floor

--- FlickerLight.gd
+++ FlickerLight.gd
@@ -4,11 +4,11 @@ var rng = RandomNumberGenerator.new();
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	light_color = Color8(180,142,73)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	light_color = Color8(210,142,73)
 	light_energy = (light_energy + rng.randf_range(0.7,1.3))/1.66
 	pass
