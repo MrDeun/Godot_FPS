12cf00c30db14d8214e5a9685309849374362446
Patryk Michalak <pmichalak16@gmail.com>
2024-05-28 00:38:38 +0200

    DAMN IT, I FORGOT

--- subscenes/orc_warrior.gd
+++ subscenes/orc_warrior.gd
@@ -2,8 +2,10 @@ extends CharacterBody3D
 
 var health = 100
 var target = null
+var state_machine
 
-const SPEED = 5.0
+const SPEED = 2.0
+const ATTACK_RANGE = 2.5
 const JUMP_VELOCITY = 4.5
 
 @onready var player_path = $"../new_player"
@@ -20,14 +22,27 @@ func got_hit(damage: float):
 		queue_free()
 	return
 
+func _ready():
+	state_machine = anim_tree.get("parameters/playback")
+func _target_in_range():
+	return global_position.distance_to(player_path.global_position) <= ATTACK_RANGE
+
 func _physics_process(delta):
 	velocity = Vector3.ZERO
+	match state_machine.get_current_node():
+		"root|walking":
+			nav_agent.set_target_position(player_path.global_transform.origin)
+			var next_nav_point = nav_agent.get_next_path_position()
+			velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
+			look_at(Vector3(global_position.x + velocity.x ,global_position.y,global_position.z + velocity.z),Vector3.UP)			
+			pass
+		"root|attackA":
+			look_at(Vector3(player_path.global_position.x,global_position.y,player_path.global_position.z),Vector3.UP)
+			pass	
+		
+	anim_tree.set("para",_target_in_range())
+	anim_tree.set("parameters/conditions/run", !_target_in_range())
 	
-	#NAVI
-	nav_agent.set_target_position(player_path.global_transform.origin)
-	var next_nav_point = nav_agent.get_next_path_position()
-	velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
-	
-	look_at(Vector3(player_path.global_position.x,global_position.y,player_path.global_position.z))
+	state_machine = anim_tree.get("parameters/playback")
 	move_and_slide()
 	
